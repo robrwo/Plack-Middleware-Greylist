9bfcb88ad2749449b74c3a982325ceec9ba7d4f3
Robert Rothenberg <rrwo@cpan.org>
2023-12-05 11:14:35 +0000

    Commit Changes and bump $VERSION

--- lib/Plack/Middleware/Greylist.pm
+++ lib/Plack/Middleware/Greylist.pm
@@ -19,7 +19,7 @@ use Plack::Util::Accessor qw/ default_rate rules cache file _match greylist retr
 use Ref::Util             qw/ is_plain_arrayref /;
 use Time::Seconds         qw/ ONE_MINUTE /;
 
-our $VERSION = 'v0.6.0';
+our $VERSION = 'v0.6.1';
 
 =head1 SYNOPSIS
 
